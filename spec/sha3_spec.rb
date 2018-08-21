f5e939a157303fa8cd31de040a322ba36ebb2ec4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-08-21 19:42:04 +0100

    Another SHA3 test case

--- spec/sha3_spec.rb
+++ spec/sha3_spec.rb
@@ -7,4 +7,12 @@ describe SHA3 do
       is_expected.to eq("a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a")
     end
   end
+
+  describe do
+    let(:msg) { "Hello, world!" }
+    it do
+      is_expected.to eq("f345a219da005ebe9c1a1eaad97bbf38a10c8473e41d0af7fb617caa0c6aa722")
+    end
+  end
+
 end
