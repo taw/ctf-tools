2a04353fdc1642213a125076b4051e299a5205df
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-08-29 01:12:24 +0100

    Synced with one in cryptopals

--- lib/gcm.rb
+++ lib/gcm.rb
@@ -60,7 +60,7 @@ module GCM
       end
 
       # Plaintext
-      each_message_block(pt, key, iv) do |block, ctr_block|
+      each_message_block_ctr(pt, key, iv) do |block, ctr_block|
         ct_block = block.xor(ctr_block)
         ct << ct_block
         ctval = ct_block + "\x00".b * (16 - block.size)
@@ -87,30 +87,33 @@ module GCM
       end
 
       # Plaintext
-      each_message_block(ct, key, iv) do |block, ctr_block|
+      each_message_block_ctr(ct, key, iv) do |block, ctr_block|
         pt_block = block.xor(ctr_block)
         pt << pt_block
         ctval = block + "\x00".b * (16 - block.size)
         tag = mul(h, tag ^ ctval.to_hex.to_i(16))
       end
 
-      final_block = (aad.bytesize*8) * (2**64) | ct.bytesize*8
-      tag = mul(h, tag^final_block)
+      tag = mul(h, tag^final_block(aad, ct))
       tag ^= iv_block(key, iv)
 
       [aad, pt, tag]
     end
 
-    private def calculate_h(key)
+    def final_block(aad, ct)
+      (aad.bytesize*8) * (2**64) | ct.bytesize*8
+    end
+
+    def calculate_h(key)
       zero_block = "\x00".b*16
       encrypt_block(key, zero_block).to_hex.to_i(16)
     end
 
-    private def iv_block(key, iv)
+    def iv_block(key, iv)
       encrypt_block(key, counter_block(iv, 1)).to_hex.to_i(16)
     end
 
-    private def each_aad_block(aad)
+    def each_aad_block(aad)
       i = 0
       while i*16 < aad.size
         block = aad[i*16, 16]
@@ -120,22 +123,28 @@ module GCM
       end
     end
 
-    # PT / CT - yield blocks and corresponding counter blocks
-    private def each_message_block(message, key, iv)
+    def each_message_block(message)
       j = 0
       while j*16 < message.size
         block = message[j*16, 16]
-        ctr_block = encrypt_block(key, counter_block(iv, j+2))
-        yield(block, ctr_block[0, block.size])
+        yield(block, j)
         j += 1
       end
     end
 
-    private def counter_block(iv, i)
+    # PT / CT - yield blocks and corresponding counter blocks
+    def each_message_block_ctr(message, key, iv)
+      each_message_block(message) do |block, j|
+        ctr_block = encrypt_block(key, counter_block(iv, j+2))
+        yield(block, ctr_block[0, block.size])
+      end
+    end
+
+    def counter_block(iv, i)
       iv + [i].pack("N")
     end
 
-    private def encrypt_block(key, block)
+    def encrypt_block(key, block)
       raise unless block.size == 16 and key.size == 16
       crypt = OpenSSL::Cipher.new("AES-128-ECB")
       crypt.encrypt
